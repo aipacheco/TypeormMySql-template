35b3476c358b13c218ca841a9c2f73cde86cc5dd
aipacheco <kornetera@gmail.com>
2024-02-27 09:37:22 +0100

    deleted some migrations

--- src/models/db.ts
+++ src/models/db.ts
@@ -3,10 +3,7 @@ import "dotenv/config"
 import { DataSource } from "typeorm"
 import { Roles1708952337364 } from "./migrations/Roles"
 import { Users1708948450323 } from "./migrations/Users"
-import { Authors1708949757263 } from "./migrations/Authors"
-import { Books1708950416085 } from "./migrations/Books"
-import { Favorites1708958513811 } from "./migrations/Favorites"
-import { Loans1708959226863 } from "./migrations/Loans"
+
 
 //aqui van las tablas, en migrations, para poderlas usar posteriormente
 export const AppDataSource = new DataSource({
@@ -19,11 +16,7 @@ export const AppDataSource = new DataSource({
   entities: [],
   migrations: [
     Roles1708952337364,
-    Users1708948450323,
-    Authors1708949757263,
-    Books1708950416085,
-    Favorites1708958513811,
-    Loans1708959226863
+    Users1708948450323
   ],
   synchronize: false,
   logging: false,
