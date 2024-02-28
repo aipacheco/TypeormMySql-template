04cc0049cea9e0cb466228afddaabeb08e5438fa
aipacheco <kornetera@gmail.com>
2024-02-28 09:37:44 +0100

    feat:new validation: if the body is empty

--- src/domain/roles/router.ts
+++ src/domain/roles/router.ts
@@ -4,6 +4,23 @@ import * as Controller from "./controller"
 //se recibe request desde el front y se le devuelve una response que va a venir desde controller
 export const createRole = async (req: Request, res: Response) => {
   console.log(req.body)
-  Controller.createRole(req, res)
-  return res
+  //si hay body y las keys vienen rellenas (no es un objeto vacío)
+  if (req.body && Object.keys(req.body).length !== 0) {
+    try {
+      await Controller.createRole(req, res)
+    } catch (error) {
+      //error del servidor
+      console.error(error)
+      res.status(500).json({
+        success: false,
+        message: "An error occurred while creating the role.",
+      })
+    }
+  } else {
+    //si viene vacío
+    res.status(400).json({
+      success: false,
+      message: "No data provided.",
+    })
+  }
 }
