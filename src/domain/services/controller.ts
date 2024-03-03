e915253938509d94716cf7266b58c5fe384377b7
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2024-03-03 12:31:59 +0100

    feat: get all services

--- src/domain/services/controller.ts
+++ src/domain/services/controller.ts
@@ -1,12 +1,26 @@
 import { Request, Response } from "express"
 import * as Repository from "./repository"
 
-export const getServices = () =>{
+export const getServices = async (req: Request, res: Response) =>{
 
+    try {
+        const resultado = await Repository.getServices()
+  
+        return res.status(200).json({
+          success: true,
+          message: "All services",
+          data: resultado,
+        })
+      } catch (error) {
+        return res.status(500).json({
+          success: false,
+          message: "Error interno del servidor",
+        })
+      }
 }
-export const createService = () =>{
+export const createService = (req: Request, res: Response) =>{
     
 }
-export const updateService = () =>{
+export const updateService = (req: Request, res: Response) =>{
     
 }
--- src/domain/services/repository.ts
+++ src/domain/services/repository.ts
@@ -1,3 +1,13 @@
 import { Request } from "express"
 import { Services } from "../../models/Services"
 
+export const getServices = async () => {
+  const services = await Services.find({
+    select: {
+      id: true,
+      serviceName: true,
+      description: true,
+    },
+  })
+  return services
+}
