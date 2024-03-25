b536ed49c0acbf146e3ae54d3dabbf0db7b5c326
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2024-03-25 10:05:54 +0100

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import express, { Application } from "express"
+import cors from "cors"
 import router from "./router"
-
 
 export const app: Application = express()
 
-app.use(express.json()) //para convertir a json los datos recibidos
+app.use(cors())
 
+app.use(express.json()) //para convertir a json los datos recibidos
 
 app.get("/hello", (req, res) => {
   res.status(200).json({ success: true, msg: "server is ok" })
 })
 
-app.use('/api', router)
+app.use("/api", router)
